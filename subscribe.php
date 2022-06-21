c33f7cb70511d171822b2c19a211a860df7d75b6
Fitting Room <fittingroom@Fittings-MacBook-Pro.local>
2022-06-21 15:37:07 +0400

    pushing first time

---
+++ subscribe.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$recipient = "saravananitbtech@gmail.com";
+
+$subject = "The Fitting Room - Subscribe from coming soon page";
+
+$location = "https://sarva.digital/fittingroom/";
+
+
+$sender = $recipient;
+
+# MAIL BODY
+$body .= "Name: New Subscriber"." \n";
+$body .= "Email: ".$_REQUEST['Email']." \n";
+# add more fields here if required
+
+## SEND MESSGAE ##
+
+
+
+mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
+
+## SHOW RESULT PAGE ##
+
+header( "Location: $location" );
+?>
